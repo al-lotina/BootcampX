4b491e2bf8f9c538b3bb8dbeeba310fec79304ac
Al Lotina <lotina.al@gmail.com>
2020-10-13 22:26:30 +0000

    added 4_queries

---
+++ 4_queries/9_cohort_avg_assist_duration.sql
@@ -0,0 +1,8 @@
+SELECT avg(total_duration) as average_total_duration -- accessing the total_duration column from the sub query
+FROM 
+(SELECT cohorts.name, sum(completed_at - started_at) as total_duration
+FROM assistance_requests
+JOIN students ON student_id = students.id 
+JOIN cohorts ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+ORDER BY total_duration) as total_durations;
